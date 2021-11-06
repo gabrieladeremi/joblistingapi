732cbfd77ef739d49c0e5a157b6756423014ab97
Amos Aderemi <gabrieladeremi.com>
2021-11-06 13:34:58 +0100

    created migrations for user and businesses

---
+++ joblisting/database/migrations/2021_11_06_112423_create_businesses_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusinessesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('businesses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('business_id');
+            $table->string('business_name');
+            $table->string('email');
+            $table->text('address');
+            $table->string('phone_number');
+            $table->string('business_type');
+            $table->timestamps();
+
+            $table->foreign('business_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('businesses');
+    }
+}
